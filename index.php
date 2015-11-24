f2155f09416c0a2897805a5838f8fd823af71620
ihptru <ihptru@gmail.com>
2015-11-24 15:28:17 +0300

    add yandex metrika counter

--- index.php
+++ index.php
@@ -464,7 +464,39 @@ else
 
 	echo "</body></html>";
 }
+?>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter33786919 = new Ya.Metrika({
+                    id:33786919,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/33786919" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<?php
 echo '
 <!-- Start of StatCounter Code for Default Guide -->
 <script type="text/javascript">
